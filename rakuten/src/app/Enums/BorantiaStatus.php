3542d22fbffb24338f2f44f0f3c0a36d45db722e
teruikun <misonomisoshiru@gmail.com>
2025-09-04 17:03:01 +0900

    Delete rakuten/src/app directory

--- rakuten/src/app/Enums/BorantiaStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Enums;
-
-enum BorantiaStatus: string
-{
-    case Recruiting = 'recruiting';
-    case Closed = 'closed';
-    case Cancelled = 'cancelled';
-}
--- rakuten/src/app/Enums/CarRequirement.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Enums;
-
-enum CarRequirement: string
-{
-    case Must = 'must';
-    case Preferred = 'preferred';
-    case None = 'none';
-}
